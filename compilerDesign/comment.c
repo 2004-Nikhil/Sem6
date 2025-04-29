4706ea3d1482d62b05711906bcbd2c14f1963114
Nikhil Nawani <115881621+2004-Nikhil@users.noreply.github.com>
2025-04-29 08:19:39 +0000

    compiler design till dfa

---
+++ compilerDesign/comment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// This is a single-line comment
+int main() {
+    /* This is a multi-line comment
+       that spans multiple lines */
+    printf("Hello, World!\n"); // Print Hello, World!
+    
+    // Another single-line comment
+    return 0; /* Return success */
+}
+
---
+++ compilerDesign/output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+    /* This is a multi-line comment
+       that spans multiple lines */
+    printf("Hello, World!\n"); 
+    
+    
+    return 0; /* Return success */
+}
+
